d9819cdd38044e5808c1f6d0fe53f8ab650b628c
Burak Kıymaz <iletisim@burakkiymaz.com>
2017-07-29 12:11:47 +0300

    added a little changes and comment lines.

--- installer.sh
+++ installer.sh
@@ -6,16 +6,17 @@ function init(){
     echo
     echo "First step! I'm searching your programs to install. "
     list_programs
-#    install_check
+    install_check
     installer
+    exit 0
 }
-
+#This function is asking to user "are you sure?"
 function install_check() {
     echo -n 'Programs on above will install. Do you really want? [Y/n]'
     read choice
     if [[ $choice == [Yy] || $choice == '' ]];
     then
-        echo 'You selected "YES". Your programs will install as soon as...'
+        echo 'You selected "YES". Your programs will install as soon as possible...'
     elif [[ $choice == [Nn] ]];
     then
         echo 'You selected "NO". Please be sure when you call me.'
@@ -25,7 +26,7 @@ function install_check() {
         exit 1
     fi
 }
-
+#List programs which will install.
 function list_programs() {
     echo '---------------------------------------------------------------------'
     echo 'Program List:'
@@ -34,19 +35,20 @@ function list_programs() {
         done
     echo '---------------------------------------------------------------------'
 }
-
+# Program Installer
 function installer() {
     echo
     echo "Step two! Your programs are installing. Please be patient dear.. "
     echo
-    for i in $( ls programs/ ); do
+    for i in $( ls programs/ ); do # for loop for install all of programs inside programs folder.
             echo "|---" $i "installing.."
-            chmod +x programs/$i
-            ./programs/$i
+            chmod +x programs/$i # this command gives execute permission to this program.
+            ./programs/$i # run command
+            echo "|---" $i "installed.."
         done
     echo
     echo "Third step! I will install... "
-    echo "I'm just kidding there is no third step... :D "
+    echo "I'm just kidding there is no third step... :D All of your programs are installed."
     echo
     echo "Enjoy with your installed programs. "
     echo
