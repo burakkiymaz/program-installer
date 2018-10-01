f8c9f019387ab70e15c484291c56931943df0506
just <iletisim@burakkiymaz.com>
2018-10-01 14:09:18 +0300

    *.deb file support added

--- installer.sh
+++ installer.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
 
-function init(){
-    echo "Hello again sweet heart :)"
-    echo "I know you deleted all of your programs again. But don't worry, I will install them for you."
-    echo
-    echo "First step! I'm searching your programs to install. "
-    list_programs
-    install_check
-    installer
-    echo
-    echo "Third step! I will install... "
-    echo "I'm just kidding there is no third step... :D All of your programs are installed."
-    echo
-    echo "Enjoy with your installed programs. "
-    echo
-
-    exit 0
-}
 #This function is asking to user "are you sure?"
 function install_check() {
-    echo -n 'Programs on above will install. Do you really want? [Y/n]'
+    echo -n 'Programs listed above. Can I install all of them? [Y/n]'
     read choice
     if [[ $choice == [Yy] || $choice == '' ]];
     then
-        echo 'You selected "YES". Your programs will install as soon as possible...'
+        echo -e 'You selected \e[1m\e[34m"YES"\e[0m\e[39m.'
+        echo
+        echo "Step two! I'm starting now. While process you can get a cup of coffee :)"
+        
     elif [[ $choice == [Nn] ]];
     then
-        echo 'You selected "NO". Please be sure when you call me.'
+        echo -e 'You selected \e[1m\e[31m"NO"\e[0m\e[39m. Please be sure when you call me.'
         exit 0
     else
         echo 'You typed the wrong letter. Exitting...'
@@ -35,27 +21,55 @@ function install_check() {
 }
 #List programs which will install.
 function list_programs() {
-    echo '---------------------------------------------------------------------'
-    echo 'Program List:'
+    echo -e '\e[1m\e[33m---------------------------------------------------------------------\e[0m\e[39m'
+    echo -e '\e[1m\e[33mProgram List:\e[0m\e[39m'
     for i in $( ls programs/ ); do
-            echo "|---" $i
+            echo -e "\e[93m|--- ${i%.*}\e[39m"
         done
-    echo '---------------------------------------------------------------------'
+    echo -e '\e[1m\e[33m---------------------------------------------------------------------\e[0m\e[39m'
 }
 # Program Installer
 function installer() {
     echo
-    echo "Step two! Your programs are installing. Please be patient dear.. "
-    echo
     for i in $( ls programs/ ); do # for loop for install all of programs inside programs folder.
-            echo "|---" $i "installing.."
-            chmod +x programs/$i # this command gives execute permission to this program.
-            ./programs/$i # run command
-            echo "|---" $i "installed.."
+            echo -e "\e[1m\e[32m|--- ${i%.*} installing..\e[0m\e[39m"
+
+            echo "File type: ${i##*.}"
+            
+            if [[ ${i##*.} == [Ss][Hh] ]];
+            then
+                chmod +x programs/$i # this command gives execute permission to this program.
+                ./programs/$i # run command
+                echo -e "\e[1m\e[34m|--- " ${i%.*} "installed..\e[39m\e[0m"
+            
+            elif [[ ${i##*.} == [Dd][Ee][Bb] ]];
+            then
+                sudo dpkg -i ./programs/$i
+                echo -e "\e[1m\e[34m|--- " ${i%.*} "installed..\e[39m\e[0m"
+            
+            else
+                echo -e "\e[1m\e[31mFile format error!\e[0m\e[39m"
+                echo -e "\e[1m\e[31m|--- " ${i%.*} "failed..\e[39m\e[0m"
+            fi
+
+            
             echo
-            echo "#####################################################################"
             echo
         done
+}
+
+
+function init(){
+    echo "Hello sweet heart :)"
+    echo "I know you deleted all of your programs again. But don't worry, I will help you to install them again."
+    echo
+    echo "First step! I'm checking your programs to install. "
+    
+    list_programs
+    install_check
+    installer
+    
+    exit 0
 }
 
 # Program Starting Point
